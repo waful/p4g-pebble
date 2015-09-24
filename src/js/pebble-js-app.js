cadcff18693e20d62bd0225eb2621c1b182a1666
Wanfung Lee <cloudwindhk@gmail.com>
2015-09-24 08:55:25 -0700

    fixing iphone issue

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -84,7 +84,7 @@ function locationError(err) {
 function getWeather() {
     // use cache if last fetch is younger than 15 minutes
     var lastFetchDate = Date.parse(localStorage.getItem("last_fetch_date"));
-    var lastFetchConditions = Number.parseInt(localStorage.getItem("last_fetch_conditions"));
+    var lastFetchConditions = parseInt(localStorage.getItem("last_fetch_conditions"));
     if(lastFetchDate && lastFetchConditions >= 0){
         console.log("using cached conditions: " + lastFetchConditions + " " + localStorage.getItem("last_fetch_details"));
         sendToApp(lastFetchConditions);
